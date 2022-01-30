6b4839dfb8a8855f9176696ca273098ce8afadbb
Michael Redifer <mikeredifer@yahoo.com>
2022-01-30 12:42:32 -0600

    Created ATMApp branch and ATM class

---
+++ CSF1Homework/ATM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class ATM
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }//end main
+    }//end class
+}//end namespace
